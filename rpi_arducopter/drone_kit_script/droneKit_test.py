2e58fcbb7eae9c970733d38199ba284d30f877b6
Captluke2328 <lukasjohnny2328@gmail.com>
2023-02-19 02:25:59 +0800

    -delete redundant files

--- rpi_arducopter/drone_kit_script/droneKit_test.py
+++
@@ -1,4 +0,0 @@
-import dronekit
-vehicle = dronekit.connect(ip="/dev/ttyAMA0", baud=115200, wait_ready=True)
-#vehicle = dronekit.connect(ip="/dev/ttyAMA0", baud=57600, wait_ready=True)
-print(vehicle.mode.name)
